d4a53572879216a13aeeae4d04329a28e43e7fd5
nuttakit-vy <56815700+NuttakitDW@users.noreply.github.com>
2022-05-25 20:14:03 +0700

    Create SimpleToken.sol

---
+++ SimpleToken.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+
+    string public name;
+    string public ticker;
+    uint256 public totalSuply;
+
+    mapping(address => uint256) public ledger;
+
+    constructor(string memory _name, string memory _ticker) {
+
+        name = _name;
+        ticker = _ticker;
+        totalSuply = 1000;
+
+        ledger[msg.sender] = 1000;
+
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        totalSuply += _amount;
+        ledger[_to] += _amount;
+    }
+
+    function burn(uint256 _amount) public {
+        totalSuply -= _amount;
+        ledger[msg.sender] -= _amount;
+    }
+
+    function transfer(address _to, uint256 _amount) public {
+        ledger[msg.sender] -= _amount;
+        ledger[_to] += _amount;
+    }
+
+}
